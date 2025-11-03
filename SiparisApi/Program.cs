d46cc0bf14b754875d3d45b3cc88302be40e9d5c
bilalgurselb <bilalgurselb@gmail.com>
2025-11-03 17:54:53 +0300

    _LAyout_Program

--- SiparisApi/Program.cs
+++ SiparisApi/Program.cs
@@ -76,6 +76,7 @@ app.UseCors("Default");
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
+/*
 // 🚫 Login olmadan doğrudan erişimi engelle
 app.Use(async (context, next) =>
 {
@@ -156,7 +157,7 @@ button:hover {
 
     await next();
 });
-
+*/
 if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
